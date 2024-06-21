b77d507c689e377feac585824976b9e897e6a66e
Nick Larsen <10092581+NickLarsenNZ@users.noreply.github.com>
2024-06-21 13:53:01 +0200

    docs(on_error): explain what it is useful for

--- src/errors.rs
+++ src/errors.rs
@@ -5,6 +5,11 @@ pub trait OnError<T, E> {
 }
 
 impl<T, E> OnError<T, E> for Result<T, E> {
+    /// Run a closure when the value is `[Result::Err]`, returning a `[Maybe]`.
+    ///
+    /// This is useful when you don't need the `[Result::Ok]` value, and gets
+    /// around the `#[must_use]` requirement that generally leads to throwing
+    /// away the result with `let _ =`.
     fn on_error<F, O: FnOnce(E) -> F>(self, op: O) -> Maybe<T, F> {
         match self {
             Ok(t) => Maybe::A(t),
